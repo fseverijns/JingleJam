95787df53f57ca4ceda6c52a41e5514ba7602262
frankseverijns <frank.severijns.fs@gmail.com>
2019-12-14 14:27:41 +0100

    Singleton base class

---
+++ Assets/Scripts/Managers/PersistentSingleton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PersistentSingleton<T> : MonoBehaviour where T : Component
+{
+    private T _instance;
+
+    public T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+                if (_instance == null)
+                {
+                    GameObject singleton = new GameObject(typeof(T).ToString());
+                    singleton.AddComponent<T>();
+                }
+                DontDestroyOnLoad(this.gameObject);
+            }
+            return _instance;
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : Component
+{
+
+    private T _instance;
+
+    public T Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+                if(_instance == null)
+                {
+                    GameObject singleton = new GameObject(typeof(T).ToString());
+                    singleton.AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+}
