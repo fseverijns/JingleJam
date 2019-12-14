4f6fa835af7ed1d3554c77a6557aaab19614bd40
Richard Janssen <richardjanssen98@gmail.com>
2019-12-14 16:53:10 +0100

    Finished Menu Controls for Controller

--- Assets/Scripts/Managers/MenuControllerManager.cs
+++ Assets/Scripts/Managers/MenuControllerManager.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 public class MenuControllerManager : PersistentSingleton<MenuControllerManager>
 {
     bool mainMenu = true;
+    int currentItem;
+
+    bool axisInUse = false;
 
     public Button[] mainMenuButtons;
     public Button[] gameMenuButtons;
@@ -24,15 +27,56 @@ public class MenuControllerManager : PersistentSingleton<MenuControllerManager>
 
     void Start()
     {
+        currentItem = 0;
+
         if (mainMenu)
         {
-            //mainMenuButtons[0];
+            mainMenuButtons[currentItem].Select();
+        }
+        else
+        {
+            gameMenuButtons[currentItem].Select();
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetAxis("MenuVertical") > 0 || Input.GetAxis("Player1Vertical") < 0)
+        {
+            if (!axisInUse)
+            {
+                axisInUse = true;
+
+                currentItem--;
+                if (currentItem < 0)
+                {
+                    currentItem = 0;
+                }
+            }
         
+        }
+        else if (Input.GetAxis("MenuVertical") < 0 || Input.GetAxis("Player1Vertical") > 0)
+        {
+            if (!axisInUse)
+            {
+                axisInUse = true;
+
+                currentItem++;
+                if (currentItem > mainMenuButtons.Length - 1)
+                {
+                    currentItem = mainMenuButtons.Length - 1;
+                }
+            }
+            
+        }
+        else
+        {
+            axisInUse = false;
+        }
+
+        mainMenuButtons[currentItem].Select();
     }
+
+
 }
