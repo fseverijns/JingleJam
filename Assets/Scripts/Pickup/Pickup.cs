3b08ef9e350fca9a94d353257586faf9e96736e8
frankseverijns <frank.severijns.fs@gmail.com>
2019-12-14 01:09:15 +0100

    Basic movement, picking up object Player prefabs Input

---
+++ Assets/Scripts/Pickup/Pickup.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class Pickup : MonoBehaviour
+{
+    private SphereCollider sphereCollider;
+    private bool pickedUpByPlayer = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sphereCollider = GetComponent<SphereCollider>();
+        sphereCollider.isTrigger = true;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PickUp(Player player)
+    {
+        pickedUpByPlayer = true;
+        sphereCollider.enabled = false;
+        StartCoroutine(MoveToPlayer(player));
+    }
+
+    public void Drop(Player player)
+    {
+        pickedUpByPlayer = false;
+        sphereCollider.enabled = true;
+        StopCoroutine(MoveToPlayer(player));
+    }
+
+    private IEnumerator MoveToPlayer(Player player)
+    {
+        while(true)
+        {
+            transform.position = Vector3.Lerp(transform.position, player.PickupHeldPosition, Time.deltaTime * 10);
+            if(Vector3.Distance(transform.position, player.PickupHeldPosition) < 0.1f)
+            {
+                transform.position = player.PickupHeldPosition;
+                yield return null;
+            }
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("ID")]
+    [SerializeField]
+    private int playerNum = 1;
+
+    [Header("Movement")]
+    [SerializeField]
+    private float speed = 5;
+
+    [Header("Pickup")]
+    [SerializeField]
+    private Vector3 pickupHeldPosition = new Vector3(0, 1, 0);
+
+    public Vector3 PickupHeldPosition { get => transform.position + pickupHeldPosition; }
+
+    private List<Pickup> pickupsInRange = new List<Pickup>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame    
+    void Update()
+    {
+        UpdateMovement();
+
+        if(Input.GetButtonDown("Player" + playerNum + "Interact"))
+        {
+            Debug.Log("Pickup!");
+            PickupObject();
+        }
+    }
+
+    void UpdateMovement()
+    {
+        float horizontalMovement = Input.GetAxis("Player" + playerNum + "Horizontal");
+        float verticalMovement = Input.GetAxis("Player" + playerNum + "Vertical");
+
+        Vector3 destination = transform.position;
+
+        destination = transform.position + Vector3.right * (horizontalMovement * speed);
+        destination += Vector3.forward * -(verticalMovement * speed);
+
+        Vector3 newPosition = Vector3.Lerp(transform.position, destination, Time.deltaTime);
+        transform.position = newPosition;
+    }
+
+    void PickupObject()
+    {
+        if (pickupsInRange.Count > 0)
+        {
+            pickupsInRange[0].PickUp(this);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag.Equals("Pickup"))
+        {
+            Debug.Log("Pickup in Range");
+            Pickup pickup = other.GetComponent<Pickup>();
+            if(pickup != null)
+            {
+                pickupsInRange.Add(pickup);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Pickup"))
+        {
+            Debug.Log("Pickup out of Range");
+            Pickup pickup = other.GetComponent<Pickup>();
+            if (pickup != null)
+            {
+                pickupsInRange.Remove(pickup);
+            }
+        }
+    }
+}
